6ca0410d9c0c2168544d1c03623f73e0ed30e874
Garam Kim <0410garam@naver.com>
2019-11-06 10:18:44 +0900

    added number

--- src/Example.java
+++ src/Example.java
@@ -4,5 +4,6 @@ public class Example {
 	public static void main(String[] args) {
 		System.out.println("ComputerSW");
 		System.out.println("Wonkwang Univ.");
+		System.out.println("20162872");
 	}
 }
